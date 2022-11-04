60d6b6fd041f1e140f32b28f75d825c49ae30662
zoki <tosevzoran@gmail.com>
2022-11-04 13:24:57 +0100

    add a dummy comment

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ type MyEvent struct {
 	Name string `json:"name"`
 }
 
+// Lambda handler function
 func HandleRequest(ctx context.Context, event MyEvent) (string, error) {
 	return fmt.Sprintf("Hello %s!", event.Name), nil
 }
